944443dfa2cf63297ee133c780149cd77109d214
Nolan J Tait <nolanjtait@gmail.com>
2022-03-01 00:09:28 -0800

    Initial commit

---
+++ spec/notebroker_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Notebroker do
+  it "has a version number" do
+    expect(Notebroker::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
